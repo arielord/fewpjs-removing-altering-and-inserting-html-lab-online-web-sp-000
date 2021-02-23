4062e4701011f998540506f9ad8ac5820ca5f9e7
Ariel <arielord@buffalo.edu>
2021-02-23 18:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 let element = document.querySelector("#main")
 element.remove()
 
-let newHeader = document.createElement('h1#victory')
+let newHeader = document.createElement('h1')
 document.body.appendChild(newHeader)
